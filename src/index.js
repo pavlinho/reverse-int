1a7333ea04ead71cf317e3de8af7047e3b3a0cca
Pavlin <60567547+pavlinho@users.noreply.github.com>
2020-03-23 22:49:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let str = String(Math.abs(n));
+  let mas = str.split('');
+  mas.reverse();
+  str = "";
+  mas.forEach((el,i) => str += el);
+  return parseInt(str);
 }
